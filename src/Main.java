88f20e8a91e4160539b8910c7f388c35a6058179
xpycteam88 <sicuno@mail.ru>
2023-09-29 22:30:25 +0300

    added variable currency

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Система расчёта штрафов");
 
-        int carSpeed = 65;
+        int carSpeed = 85;
 
         int fineFor20to40 = 500;
         int fineFor40to60 = 1000;
         int fineFor60to80 = 2000;
         int fineFor80andMore = 5000;
+
+        String currency;
+        currency = " рублей";
 
         int townSpeed = 60;
 
@@ -17,16 +20,16 @@ public class Main {
             System.out.println("Скорость не превышена или превышена незначительно");
         }
         else if(overSpeed >= 20 && overSpeed < 40) {
-            System.out.println("Штраф: " + fineFor20to40);
+            System.out.println("Штраф: " + fineFor20to40 + currency);
         }
         else if(overSpeed >= 40 && overSpeed < 60) {
-            System.out.println("Штраф: " + fineFor40to60);
+            System.out.println("Штраф: " + fineFor40to60 + currency);
         }
         else if(overSpeed >= 60 && overSpeed < 80) {
-            System.out.println("Штраф: " + fineFor60to80);
+            System.out.println("Штраф: " + fineFor60to80 + currency);
         }
         else if(overSpeed >= 80) {
-            System.out.println("Штраф: " + fineFor80andMore);
+            System.out.println("Штраф: " + fineFor80andMore + currency);
         }
     }
 }
