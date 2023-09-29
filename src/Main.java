f7f428acde404fca27d98a9933758ad0e16389f9
xpycteam88 <sicuno@mail.ru>
2023-09-29 22:18:51 +0300

    Added code for systemControlSpeed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Система расчёта штрафов");
+
+        int carSpeed = 65;
+
+        int fineFor20to40 = 500;
+        int fineFor40to60 = 1000;
+        int fineFor60to80 = 2000;
+        int fineFor80andMore = 5000;
+
+        int townSpeed = 60;
+
+        int overSpeed = carSpeed - townSpeed;
+
+        if(overSpeed < 20) {
+            System.out.println("Скорость не превышена или превышена незначительно");
+        }
+        else if(overSpeed >= 20 && overSpeed < 40) {
+            System.out.println("Штраф: " + fineFor20to40);
+        }
+        else if(overSpeed >= 40 && overSpeed < 60) {
+            System.out.println("Штраф: " + fineFor40to60);
+        }
+        else if(overSpeed >= 60 && overSpeed < 80) {
+            System.out.println("Штраф: " + fineFor60to80);
+        }
+        else if(overSpeed >= 80) {
+            System.out.println("Штраф: " + fineFor80andMore);
+        }
     }
 }
